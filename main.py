ad449b9dfb330b7be339c4a717e69d28193c7e27
Niklas Gerber <niklas.gerber@gmail.com>
2025-09-20 16:16:29 +0200

    Lösung als Skript fertig

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+import pandas as pd
 
+# CSV einlesen
+df = pd.read_csv("data/invoices.csv")
+
+# Erwarteten Wert berechnen
+df["expected_total"] = df["quantity"] * df["price"]
+
+# Prüfen, ob korrekt
+df["is_correct"] = df["expected_total"] == df["total"]
+
+# Ergebnis ausgeben
+print(df)
+
+# Neue Datei speichern
+df.to_csv("data/invoices_checked.csv", index=False)
